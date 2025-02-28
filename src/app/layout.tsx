79b2be748abb5938b46cc41c5c1404048be1eac1
Artur Blaya <arturblaya@gmail.com>
2025-02-28 20:21:44 +0100

    feat: add DaisyUI for improved styling and theme support
    - Installed DaisyUI as a TailwindCSS plugin
    - Updated `globals.css` to include DaisyUI with a light theme
    - Modified `layout.tsx` to set `data-theme='light'` for theme consistency
    - Updated `package.json` and `package-lock.json` with DaisyUI dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html data-theme="light" lang="en">
       <body>{children}</body>
     </html>
   );
