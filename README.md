3e922fb34ee8bc91114c7306aabea7c5251dccfb
Artur Blaya <arturblaya@gmail.com>
2025-02-28 20:02:57 +0100

    chore: initialize project with clean setup and add README
    - Set up a fresh Next.js project
    - Removed unnecessary default files for a clean start
    - Added an initial README with project description

---
+++ README.md
@@ -0,0 +1,2 @@
+# ⏳ Epoch Converter
+A simple Epoch time converter built with Next.js. ⚡ This tool allows users to convert Unix timestamps to human-readable dates and vice versa, with support for multiple time zones. 🌍
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Epoch Converter",
+  description:
+    "Convert Unix timestamps to human-readable dates with timezone support.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
