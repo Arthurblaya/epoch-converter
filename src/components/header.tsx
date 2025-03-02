79e592aeeecbbff66ed043cdd4bbae2d5a6a09f1
Artur Blaya <arturblaya@gmail.com>
2025-03-02 19:53:47 +0100

    refactor: move SVG icons to separate files for better modularity
    - Created `src/icons/clipBoardIcon.tsx` and `src/icons/clockIcon.tsx` to store SVG components.
    - Updated `CurrentEpoch.tsx`:
      - Replaced inline SVG with `ClipboardIcon` component.
      - Cleaned up indentation and removed unnecessary comments.
    - Updated `Header.tsx`:
      - Replaced inline clock SVG with `ClockIcon` component.
      - Improved readability of JSX structure.
    - Adjusted imports to reflect the new icon locations.
    
    This improves code maintainability, making it easier to reuse and modify icons independently.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,29 +1,14 @@
 import { consoleFont } from "@/app/layout";
+import ClockIcon from "@/icons/clockIcon";
 
 export default function Header() {
   return (
     <header className="p-4">
       <div className="container mx-auto flex max-w-screen-lg items-center justify-between">
         <div className="flex items-center gap-2 cursor-pointer">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7 text-primary"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
+          <ClockIcon className="size-7 text-primary" />
 
-          <h1
-            className={`${consoleFont.className} text-xl font-bold`}
-          >
+          <h1 className={`${consoleFont.className} text-xl font-bold`}>
             <span className="text-primary">Epoch</span>{" "}
             <span className="">Converter</span>
           </h1>
